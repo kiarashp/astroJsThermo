b79fc375ae7b74d64cb6aa71d14fbd9428131c08
kiarashp <kiarashpoormohammad@gmail.com>
2025-11-04 17:59:08 +0330

    updating links and env

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thermo.kiapmd.dev',
+    // site: 'https://thermo.kiapmd.dev',
+
+    site: import.meta.env.PUBLIC_SITE_URL || 'http://localhost:4321',
     vite: {
         plugins: [tailwindcss()],
         resolve: {
